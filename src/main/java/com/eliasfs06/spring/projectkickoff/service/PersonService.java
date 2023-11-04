f69b461d1f47d6d52660dc50659d9e0141afa948
Elias Silva <eliasfs0612@gmail.com>
2023-11-04 19:09:45 -0300

    first commit

---
+++ src/main/java/com/eliasfs06/spring/projectkickoff/service/PersonService.java
@@ -0,0 +1,9 @@
+package com.eliasfs06.spring.projectkickoff.service;
+
+import com.eliasfs06.spring.projectkickoff.model.Person;
+import com.eliasfs06.spring.projectkickoff.repository.GenericRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonService extends GenericService<Person>{
+}
